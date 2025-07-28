dd0eeb027adb04d186d33cc44f7ca89a5882a63e
Quinn Morrison <96103133+sqmorrison@users.noreply.github.com>
2025-07-28 02:33:09 -0500

    Create DSAConcepts.swift

---
+++ DSAHelper/Concepts/DSAConcepts.swift
@@ -0,0 +1,21 @@
+//
+//  DSAData.swift
+//  DSAHelper
+//
+//  Created by Quinn M on 7/28/25.
+//
+
+import Foundation
+
+let dsaConcepts: [DSAConcept] = [
+    DSAConcept(
+            title: "Arrays",
+            description: "Arrays are linear data structures that store elements in contiguous memory...",
+            codeExample: """
+    let nums = [1, 2, 3, 4]
+    print(nums[2]) // 3
+    """,
+            associatedProblems: ["Two Sum", "Product of Array Except Self"]
+        ),
+    // add more...
+]
