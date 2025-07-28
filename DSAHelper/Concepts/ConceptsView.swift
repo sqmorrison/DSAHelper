13390cdfcde8b874b2596cdea819aace0c456f87
Quinn Morrison <96103133+sqmorrison@users.noreply.github.com>
2025-07-28 02:33:03 -0500

    Create ConceptsView.swift

---
+++ DSAHelper/Concepts/ConceptsView.swift
@@ -0,0 +1,33 @@
+//
+//  ConceptsView.swift
+//  DSAHelper
+//
+//  Created by Quinn M on 7/28/25.
+//
+
+import SwiftUI
+
+struct ConceptsView: View {
+    var body: some View {
+        NavigationView {
+            List(dsaConcepts) { concept in
+                NavigationLink(destination: ConceptDetailView(concept: concept)) {
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text(concept.title)
+                                .font(.headline)
+                            Text("\(concept.associatedProblems.count) problems")
+                                .font(.caption)
+                                .foregroundColor(.gray)
+                        }
+                        Spacer()
+                        Image(systemName: "chevron.right")
+                            .foregroundColor(.gray)
+                    }
+                    .padding(.vertical, 4)
+                }
+            }
+            .navigationTitle("DSA Concepts")
+        }
+    }
+}
