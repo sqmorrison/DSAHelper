01684bfb0b1ce2d552085a40fc410a70f31857c6
Quinn Morrison <96103133+sqmorrison@users.noreply.github.com>
2025-07-28 02:33:40 -0500

    Create problems.swift

---
+++ DSAHelper/Tracker/problems.swift
@@ -0,0 +1,16 @@
+//
+//  problems.swift
+//  DSAHelper
+//
+//  Created by Quinn M on 7/28/25.
+//
+
+import Foundation
+
+let leetcode75: [LeetCodeProblem] = [
+    LeetCodeProblem(title: "Two Sum", difficulty: "Easy", topic: "Array"),
+    LeetCodeProblem(title: "Best Time to Buy and Sell Stock", difficulty: "Easy", topic: "Array"),
+    LeetCodeProblem(title: "Valid Anagram", difficulty: "Easy", topic: "Hash Map"),
+    LeetCodeProblem(title: "Top K Frequent Elements", difficulty: "Medium", topic: "Heap"),
+    LeetCodeProblem(title: "Valid Parentheses", difficulty: "Easy", topic: "Stack"),
+]
