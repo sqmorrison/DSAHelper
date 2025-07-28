1d25fe8b07f3a3dd260fff7c4f8961f41dd00a9e
Quinn Morrison <96103133+sqmorrison@users.noreply.github.com>
2025-07-28 02:33:41 -0500

    Create TrackerModel.swift

---
+++ DSAHelper/Tracker/TrackerModel.swift
@@ -0,0 +1,15 @@
+//
+//  TrackerConcept.swift
+//  DSAHelper
+//
+//  Created by Quinn M on 7/28/25.
+//
+
+import Foundation
+
+struct LeetCodeProblem: Identifiable, Codable {
+    var id: UUID = UUID()
+    let title: String
+    let difficulty: String
+    let topic: String
+}
